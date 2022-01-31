8928483a01e039349f141aedb74c974bc2e3e89d
Jomy10 <jonas.vbs4@gmail.com>
2022-01-31 11:29:15 +0100

    Moved package route to controller

--- Sources/App/Controllers/PackagePage.swift
+++ Sources/App/Controllers/PackagePage.swift
@@ -7,3 +7,38 @@ final class PackagePageController {
     }
 }
 */
+
+final class PackagePageController {
+    func getPage(req: Request) throws -> EventLoopFuture<View> {
+        let package_name = req.parameters.get("name")!
+        
+        let file = try String(contentsOfFile: "Data/packages.json", encoding: String.Encoding.utf8)
+        let decoder = JSONDecoder()
+        let packages: [Package] = try decoder.decode([Package].self, from: file.data(using: String.Encoding.utf8)!)
+
+        // return PackagePageController().getPage(req: req) 
+        // Search package in JSON file
+        if var package = packages.first(where: { $0.title == package_name }) {
+            if package.desc == nil {
+                // Get readme from repo
+                if package.repo != nil {
+                    var readme_link: String = package.repo!
+                    readme_link = readme_link.replacingOccurrences(of: "https://github.com", with: "https://raw.githubusercontent.com")
+                    // readme_link = readme_link?.replacingOccurrences(of: ".git", with: "")
+                    readme_link.append("/master/README.md")
+                    
+                    // Get content from link and append to desc
+                    let contents = try String(contentsOf: URL(string: readme_link)!)
+                    
+                    // TODO: parse markdown using Apple's Swift Markdown
+                    // package.desc = parse_md(contents)
+                    // package.desc = (build html from markdown)
+                    package.desc = parse(readme: contents)
+                }
+            }
+            return req.view.render("package", package)
+        } else {
+            return req.view.render("package_not_found", ["title": package_name])
+        }
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -14,41 +14,16 @@ func routes(_ app: Application) throws {
         let file = try String(contentsOfFile: "Data/packages.json", encoding: String.Encoding.utf8)
         let decoder = JSONDecoder()
         let packages: [Package] = try decoder.decode([Package].self, from: file.data(using: String.Encoding.utf8)!) 
-        
+
         return req.view.render("index", ["packages": packages])
+    }
+
+    app.get("category", ":category") { req -> EventLoopFuture<View> in 
+        return req.view.render("not_found")
     }
     
     /// Packages
     app.get("package", ":name") { req -> EventLoopFuture<View> in
-        let package_name = req.parameters.get("name")!
-        
-        let file = try String(contentsOfFile: "Data/packages.json", encoding: String.Encoding.utf8)
-        let decoder = JSONDecoder()
-        let packages: [Package] = try decoder.decode([Package].self, from: file.data(using: String.Encoding.utf8)!)
-
-        // return PackagePageController().getPage(req: req) 
-        // Search package in JSON file
-        if var package = packages.first(where: { $0.title == package_name }) {
-            if package.desc == nil {
-                // Get readme from repo
-                if package.repo != nil {
-                    var readme_link: String = package.repo!
-                    readme_link = readme_link.replacingOccurrences(of: "https://github.com", with: "https://raw.githubusercontent.com")
-                    // readme_link = readme_link?.replacingOccurrences(of: ".git", with: "")
-                    readme_link.append("/master/README.md")
-                    
-                    // Get content from link and append to desc
-                    let contents = try String(contentsOf: URL(string: readme_link)!)
-                    
-                    // TODO: parse markdown using Apple's Swift Markdown
-                    // package.desc = parse_md(contents)
-                    // package.desc = (build html from markdown)
-                    package.desc = parse(readme: contents)
-                }
-            }
-            return req.view.render("package", package)
-        } else {
-            return req.view.render("package_not_found", ["title": package_name])
-        }
+        return try PackagePageController().getPage(req: req)
     }
 }
