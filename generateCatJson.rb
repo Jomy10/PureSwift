e85884861705e762e7bc47e5d8cdb999e148051b
Jomy10 <jonas.vbs4@gmail.com>
2022-02-05 17:11:31 +0100

    added json generate script for categories + started work on category page

--- generateCatJson.rb
+++ generateCatJson.rb
@@ -1,3 +1,29 @@
 #!/usr/bin/ruby
+require 'json'
 
-# TODO: #6 create json file containing packages per category for easier searching through categories at build time
+# Generates the json file for categories an the packages included in them
+
+CategoryContent = Struct.new(:desc, :packages)
+categories = JSON.parse(File.read("Public/Data/categories.json"))
+packages = JSON.parse(File.read("Public/Data/packages.json"))
+
+packages_in = Hash.new
+
+packages.each do |package|
+    package["categories"].each do |category| 
+        if not packages_in[category].nil?
+            packages_in[category].packages.append(package["title"])
+        else
+            packages_in[category] = CategoryContent.new(categories[category]["desc"], [package["title"]])
+        end
+    end
+end
+
+new_packages = Hash.new
+packages_in.each_with_index do |val|
+    new_packages[val[0]] = "{\"desc\":\"#{val[1].desc}\",\"packages\":#{val[1].packages.to_json}}"
+end
+
+json = JSON.generate(new_packages).gsub("\\\"", "\"").gsub("\"{", "{").gsub("}\"", "}")
+
+File.write("Public/Data/generated/categories+packages.json", json)
